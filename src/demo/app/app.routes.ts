d4ac2e4ff47e87986ce3ad221fde97c4e5e56ba2
Hervé TINANT <herve.tinant@finquest.com>
2017-07-20 17:00:21 +0200

    fix typo

--- src/demo/app/app.routes.ts
+++ src/demo/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { FlatComponent } from './components/flat.component';
 import { HierarchicalComponent } from './components/hierarchical.component';
-import { SimpleValueComponent } from "./components/simple-value.component";
+import { SimpleValueComponent } from './components/simple-value.component';
 
 export const AppRoutes = [
   { path: '',   redirectTo: '/flat', pathMatch: 'full' },
