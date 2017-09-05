e071bfd4e127ac872151b7472b82d6e84bcef36b
Guillaume Schaeffer <guillaume.schaeffer@finquest.com>
2017-09-05 12:52:22 +0200

    merge

---
+++ src/lib/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tree_select_default_options_1 = require("./src/model/tree-select-default-options");
+exports.TreeSelectDefaultOptions = tree_select_default_options_1.TreeSelectDefaultOptions;
+var tree_select_component_1 = require("./src/component/tree-select.component");
+exports.TreeSelectComponent = tree_select_component_1.TreeSelectComponent;
+var item_pipe_1 = require("./src/pipe/item.pipe");
+exports.ItemPipe = item_pipe_1.ItemPipe;
+var isVisible_pipe_1 = require("./src/pipe/isVisible.pipe");
+exports.IsVisiblePipe = isVisible_pipe_1.IsVisiblePipe;
+var module_1 = require("./src/module");
+exports.NgxTreeSelectModule = module_1.NgxTreeSelectModule;
+//# sourceMappingURL=index.js.map
