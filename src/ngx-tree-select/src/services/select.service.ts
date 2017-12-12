5605bdc7571ab8d9b1c01238ca4f0bead37bc756
TINANT Hervé <herve.tinant@finquest.com>
2017-12-12 17:43:33 +0100

    fix: Change Angular version support
    fix: Change Angular version support (https://github.com/Crazyht/ngx-tree-select/issues/68)

--- src/ngx-tree-select/src/services/select.service.ts
+++ src/ngx-tree-select/src/services/select.service.ts
@@ -1,4 +1,4 @@
-import { Subject } from 'rxjs/Rx';
+import { Subject } from 'rxjs/Subject';
 import { SelectableItem } from '../models/selectable-item';
 import { SelectOption } from '../models/select-option';
 import { Injectable } from '@angular/core';
